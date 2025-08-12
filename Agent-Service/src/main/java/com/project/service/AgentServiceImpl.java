0999433ec59479fd3ce1a4d76599c5ec049fd1e5
nahush p <nahushpatankar17@gmail.com>
2025-08-12 10:34:35 +0530

    from nahush

---
+++ Agent-Service/src/main/java/com/project/service/AgentServiceImpl.java
@@ -0,0 +1,61 @@
+package com.project.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.model.Agent;
+import com.project.repository.AgentRepository;
+//import com.project.repository.AgentRepository;
+
+
+@Service
+public class AgentServiceImpl implements AgentService {
+	
+	@Autowired
+	private AgentRepository repo;
+
+	@Override
+	public String createAgent(Agent agent) {
+	    repo.save(agent);
+	    return "Agent saved";
+	    
+	}
+
+	@Override
+	public List<Agent> getAllAgents() {
+		return repo.findAll();
+	
+	}
+	
+
+    public Optional<Agent> getAgentById(int agentId){
+    	return repo.findById(agentId);
+    	
+    }
+
+	@Override
+	public Agent updateAgent(int agentId, Agent updateAgent) {
+		return repo.findById(agentId)
+				.map(agent -> {
+					agent.setName(updateAgent.getName());
+					agent.setContactInfo(updateAgent.getContactInfo());
+					agent.setAgentId(updateAgent.getAgentId());
+					return repo.save(agent);
+				})
+				.orElseThrow(()-> new RuntimeException("Agent not found"));
+	
+	}
+
+	@Override
+	public String deleteAgent(int agentId) {
+		
+	      repo.deleteById(agentId);
+	      return "Agent Deleted";
+	}
+	
+
+
+}
