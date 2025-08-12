1ed95d214caf0cfc16d32ea07d668189fe7a8243
Christober Lawrance S <christoberlawrance@gmail.com>
2025-08-12 11:52:51 +0530

    Claim Setup Methods

---
+++ Claim-Service/src/main/java/com/project/InsuranceClaimProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class InsuranceClaimProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClaimProcessingApplication.class, args);
+	}
+
+}
