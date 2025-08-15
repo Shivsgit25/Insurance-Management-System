75e3f40b47a08889895492dbdb4a98a8dbddafa9
2431062 <whitedevil7628@gmail.com>
2025-08-15 15:38:00 +0530

    new commit from mohit

--- Customer-Service/src/main/java/com/project/CustomerServiceApplication.java
+++ Customer-Service/src/main/java/com/project/CustomerServiceApplication.java
@@ -1,5 +1,7 @@
 package com.project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,8 +10,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(CustomerServiceApplication.class);
 
+    public static void main(String[] args) {
+        logger.info("Starting CustomerServiceApplication...");
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 }
