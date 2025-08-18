39058c87c920d0b4f397b06aac05a8ecabb5e1d1
Sarfrazsalik <zsalik2000@gmail.com>
2025-08-18 14:56:23 +0530

    Add Junit test

--- Policy-Service/src/test/java/com/project/tests/PolicyServiceApplicationTests.java
+++ Policy-Service/src/test/java/com/project/tests/PolicyServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.project.tests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerServiceApplicationTests {
+class PolicyServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
