4dc82658d5e3ace93db139c6a582e8eda20a89ff
Shiv G <shivgupta.cse14@gmail.com>
2025-09-22 09:35:46 +0530

    exceptions

---
+++ Agent-Service/src/main/java/com/project/exception/AdharcardNumberAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.project.exception;
+
+public class AdharcardNumberAlreadyExistException extends RuntimeException{
+
+	public AdharcardNumberAlreadyExistException(String msg) {
+		super(msg);
+	}
+	
+}
